ba784286a1e0016ff614f56eb54b322eac74c0ee
rovshan.huseynov <rovshan.huseynov@cybernet.az>
2026-06-23 00:36:56 +0400

    Initial commit

---
+++ src/main/java/com/local/ms/restaurant/MsRestaurantApplication.java
@@ -0,0 +1,11 @@
+package com.local.ms.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRestaurantApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsRestaurantApplication.class, args);
+    }
+}
---
+++ src/test/java/com/local/ms/restaurant/MsRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local.ms.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
