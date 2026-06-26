9dedb6dcf6c00e3cc2c006b535eee6e401d0b831
rovshan.huseynov <rovshan.huseynov@cybernet.az>
2026-06-27 00:10:54 +0400

    implement RestaurantRepository

--- src/main/java/com/local/ms/restaurant/dao/repository/RestaurantRepository.java
+++ src/main/java/com/local/ms/restaurant/dao/repository/RestaurantRepository.java
@@ -1,4 +1,7 @@
 package com.local.ms.restaurant.dao.repository;
 
-public interface RestaurantRepository {
+import com.local.ms.restaurant.dao.entity.RestaurantEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RestaurantRepository extends CrudRepository<RestaurantEntity, String> {
 }
